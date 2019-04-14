21c5ffe5cdb66d3ac5b9d3f85602a2bacc32dc21
Miguel <artisanade@gmail.com>
2019-04-14 23:04:04 +0200

    Adding readArray function

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 void copia( char *, const char *);
 int copia2( char *str1, int len, const char *str2 );
+int readArray( char *str );
 
 
 int main( void )
@@ -21,18 +22,33 @@ int main( void )
 	copia2( source, strlen( source ), destination ); 
 	printf( "After merging both strings, source contains %s\n", source );
 	
+	printf( "Reading source ... \n" );
+	readArray( source );
+	
 	return 0;
 }
 
 void copia( char *str1, const char *str2 ) 
 {
 	strcat( str1, str2 );
-	
 }
 
 
 int copia2( char *str1, int len, const char *str2 ) 
 {
 	memcpy( str1, str2, len * sizeof( char ) );
+	return 0;
+}
+
+int readArray( char *str )
+{
+	int i = 0;
+	
+	while( '\0' != str[i] ) 
+	{
+		printf( "%c ", str[i] );
+		i++;
+	}
+	printf( "\n" );
 	return 0;
 }
