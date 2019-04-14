47f9bb896cff40b73f8d8cf986908ad690af30b3
Miguel <artisanade@gmail.com>
2019-04-14 22:49:46 +0200

    New brach devel

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 
 void copia( char *, const char *);
+int copia2( char *str1, int len, const char *str2 );
+
 
 int main( void )
 {
@@ -14,11 +16,22 @@ int main( void )
 	copia( source, destination ); 
 	printf( "After merging both strings, source contains %s\n", source );
 	
+	printf( "Using 'memcpy'\n" );
+	copia2( source, strlen( source ), destination ); 
+	printf( "After merging both strings, source contains %s\n", source );
+	
 	return 0;
 }
 
-void copia(char *str1, const char *str2) 
+void copia( char *str1, const char *str2 ) 
 {
 	strcat( str1, str2 );
 	
+}
+
+
+int copia2( char *str1, int len, const char *str2 ) 
+{
+	memcpy( str1, str2, len * sizeof( char ) );
+	return 0;
 }
