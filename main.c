244f47e615379fa2458849e2f91cbad8605cae85
Miguel <artisanade@gmail.com>
2019-04-14 18:25:55 +0200

    Merging strings with strcat

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
-
+void copia( char *, const char *);
 
 int main( void )
 {
-	int a = 6;
-	char b[ ] = {'c', 'a', 'l', 'i', '\0' }; 
+	char source[ 9 ] = { 'c', 'a', 'l', 'i' }; 
+	char *destination = "mero";
 	
-	printf( "First version\n" );
-	printf( "a es %d\n", a );
-	printf( "b es %s\n", b );
+	printf( "source contains %s\n", source );
+	printf( "destination contains %s\n", destination );
+	
+	copia( source, destination ); 
+	printf( "After merging both strings, source contains %s\n", source );
 	
 	return 0;
+}
+
+void copia(char *str1, const char *str2) 
+{
+	strcat( str1, str2 );
+	
 }
