57e652a6301dd01a37d9879ae9be751cc66cbfdc
csFormation <43139064+csFormation@users.noreply.github.com>
2019-04-14 17:16:46 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int main( void )
+{
+	int a = 6;
+	char b[ ] = {'c', 'a', 'l', 'i', '\0' }; 
+	
+	printf( "First version\n" );
+	printf( "a es %d\n", a );
+	printf( "b es %d\n", b );
+	
+	return 0;
+}
